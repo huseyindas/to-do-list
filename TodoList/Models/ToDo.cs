d8273a241243adab026796a7c4e4bdf284adf400
Hüseyin <hsyndass@gmail.com>
2021-06-01 14:48:01 +0300

    first commit

---
+++ TodoList/Models/ToDo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoList.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool IsDone { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+    }
+}
